71f93fd0fa5e536b2a82a443559cda6a438dcc58
viet <123-43@ro.ru>
2020-12-25 09:29:34 +0300

    fix При некорректном вводе в любые формы после валидации заполненные ранее значения сбрасываются

--- resources/views/plastinky.blade.php
+++ resources/views/plastinky.blade.php
@@ -11,22 +11,22 @@
 
         <div class="form-group">
             <label for="name">Введите названия</label>
-            <input type="text" name="name" placeholder="Введите названия" class="form-control" id="name">
+            <input type="text" name="name" placeholder="Введите названия" class="form-control" id="name" value="{{ old('name') }}">
         </div>
 
         <div class="form-group">
             <label for="genre">Введите жанр</label>
-            <input type="text" name="genre" placeholder="Введите жанр" class="form-control" id="genre">
+            <input type="text" name="genre" placeholder="Введите жанр" class="form-control" id="genre" value="{{ old('genre') }}">
         </div>
 
         <div class="form-group">
             <label for="date">Введите дату релиза</label>
-            <input type="date" name="date" class="form-control" id="date">
+            <input type="date" name="date" class="form-control" id="date" value="{{ old('date') }}">
         </div>
 
         <div class="form-group">
             <label for="desc">Введите описание</label>
-            <textarea name="desc" id="desc" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="desc" id="desc" cols="30" rows="10" class="form-control">{{ old('desc') }}</textarea>
         </div>
 
         <button type="submit" class="btn btn-success">Добавить</button>
